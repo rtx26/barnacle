bcc83e22495fc840a14c7ba0eb8ea442c9df4c25
rtx26 <154539537+rtx26@users.noreply.github.com>
2023-12-25 11:25:47 -0600

    Add files via upload

---
+++ ide.js
@@ -0,0 +1,135 @@
+/*alert("lo as logrado");
+document.getElementsByClassName("imajen").onclick = function() {miFuncion()};
+
+function miFuncion() {
+  alert("¡Has hecho clic en el div!");
+}
+
+console.log(miFuncion())*/
+/*console.log("hola mundo")
+document.getElementsByClassName("h1").innerHTML = "holamundo";
+
+var nombre = "sergio";
+function hola(nombre){
+  return "hola " + nombre;
+
+}
+
+console.log(hola);*/
+/*<button onclick="miFuncion()">Haz clic aquí</button>
+
+<script>
+function miFuncion() {
+  alert('¡Has hecho clic en el botón!');
+}
+</script>
+*/
+
+/*function sor(){
+ alert("as echo clic");
+}
+
+
+function mifuncion(){
+  var ele = document.getElementById("elemento").value;
+  var x = "123456789";
+  var y = "987654321";
+  var z = "234567891";
+  if(x === ele){
+    alert("$150");
+  }
+
+  if(y === ele){
+    alert("$200");
+  }
+  if(z === ele){
+   alert("$300");
+  }
+}*/
+
+/* error por el else if
+function mifuncion(){
+  var ele = document.getElementById("elemento").value;
+  var x = "123456789";
+  var y = "987654321";
+  var z = "234567891";
+  var a = "345678912"
+  if(x === ele){
+    alert("$150");
+  }
+   if(y == ele){
+     alert("$200");
+   }
+   if(z === ele){
+    alert("$300");
+   }
+   if(a === ele){
+    alert("$350");
+   }
+   else{
+  alert("error");
+  }
+}*/
+
+
+/**function mifuncion(){
+  var ele = document.getElementById("elemento").value;
+  var x = "123456789";
+  var y = "987654321";
+  var z = "234567891";
+  var a = "345678912"
+  if(x === ele){
+    alert("$150");
+  }
+  else if(y === ele){
+     alert("$200");
+  }
+  else if(z === ele){
+    alert("$300");
+  }
+  else if(a === ele){
+    alert("$350");
+  }
+  else{
+    alert("error");
+  }
+}**/
+
+const confirmacion = document.querySelector(".button1");
+confirmacion.addEventListener("click", function(){
+  alert("as echo bien el trabajo");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
